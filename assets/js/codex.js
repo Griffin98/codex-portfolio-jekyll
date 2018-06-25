0c40d7f32fd9115a33bdb0bf0c9c54ac1106b108
Griffin98 <griffin98@protonmail.com>
2018-06-25 13:56:32 +0530

    codex: add snackbar for placeholders

--- assets/js/codex.js
+++ assets/js/codex.js
@@ -54,3 +54,10 @@ strings: [
 
   }
 });
+
+//SnackBar
+function showSnackBar() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
